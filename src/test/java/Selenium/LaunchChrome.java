78eee19d3bec2515a27a6d2da0956534c62a43a3
andresjoeam <akademicandrew@gmail.com>
2024-06-12 12:07:17 -0400

    creating a new driver for chrome

--- src/test/java/Selenium/LaunchChrome.java
+++ src/test/java/Selenium/LaunchChrome.java
@@ -8,6 +8,7 @@ public class LaunchChrome {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","C:\\ChromeDriverSelenium\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        WebDriver driver1 = new ChromeDriver();
         driver.get("https://www.amazon.in/");
         driver.close();
     }
