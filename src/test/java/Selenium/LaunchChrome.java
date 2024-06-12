8a1c00e2c3edd51d93687f1642537293f753a4b3
andresjoeam <akademicandrew@gmail.com>
2024-06-12 18:38:43 -0400

    Searching an item in text field and clicking on search button

--- src/test/java/Selenium/LaunchChrome.java
+++ src/test/java/Selenium/LaunchChrome.java
@@ -1,16 +1,19 @@
 package Selenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LaunchChrome {
 
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","C:\\ChromeDriverSelenium\\chromedriver-win64\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        WebDriver driver1 = new ChromeDriver();
-        driver1.get("https://www.google.com/");
-        driver.get("https://www.amazon.com/");
-        driver.close();
+        System.setProperty("webdriver.chrome.driver","C:\\ChromeDriverSelenium\\chromedriver-win64\\chromedriver.exe"); //Specidy the location in machine where the driver is located
+        WebDriver driver = new ChromeDriver();          //Creating a new object to handle chrome
+        driver.get("https://www.amazon.com");          //telling the object to open the browser in the URL
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Headphones");        //Ordering the object to find the textfield for search and write what we want
+        driver.findElement(By.id("nav-search-submit-button")).click();            //Ordering the object to click on "search" button
+        //driver.findElement(By.className("a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal")).click();
+
+
     }
 }
