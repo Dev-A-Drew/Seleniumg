1fd7b91b8c13706890872d3d323705a102940628
andresjoeam <akademicandrew@gmail.com>
2024-06-17 15:58:13 -0400

    Poinitng new page and using cssSelector for button

--- src/test/java/Selenium/LaunchChrome.java
+++ src/test/java/Selenium/LaunchChrome.java
@@ -5,15 +5,32 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LaunchChrome {
-
+     static String pass="Jamesdrew39?";
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","C:\\ChromeDriverSelenium\\chromedriver-win64\\chromedriver.exe"); //Specidy the location in machine where the driver is located
-        WebDriver driver = new ChromeDriver();          //Creating a new object to handle chrome
-        driver.get("https://www.amazon.com");          //telling the object to open the browser in the URL
-        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Headphones");        //Ordering the object to find the textfield for search and write what we want
-        driver.findElement(By.id("nav-search-submit-button")).click();            //Ordering the object to click on "search" button
-        //driver.findElement(By.className("a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal")).click();
+        WebDriver driver = new ChromeDriver();                  //Creating a new object to handle chrome
+        // for Amazon
 
+        /*
+        driver.get("https://www.amazon.com");                   //telling the object to open the browser in the URL
+        //driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Headphones");            //Ordering the object to find the textfield for search and write what we want
+        //driver.findElement(By.id("nav-search-submit-button")).click();                                  //Ordering the object to click on "search" button
+        //driver.findElement(By.className("a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal")).click();
+        driver.findElement(By.id("nav-link-accountList-nav-line-1")).click();
+        driver.findElement(By.id("ap_email")).sendKeys("andresjoeam@gmail.com");
+        driver.findElement(By.className("a-button-input")).click();
+        driver.findElement(By.name("password")).sendKeys(pass);
+        driver.findElement(By.id("signInSubmit")).click();
+        //driver.findElement(By.class(""));
+
+        */
+
+        // for other page
+        driver.get("https://parabank.parasoft.com/parabank/index.htm");
+        driver.findElement(By.name("username")).sendKeys("john");
+        driver.findElement(By.name("password")).sendKeys("demo");
+        driver.findElement(By.cssSelector(".button:nth-child(1)")).click();
+        driver.findElement(By.linkText("Log Out")).click();
 
     }
 }
