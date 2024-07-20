f26a6f8bb6ae1a8236ae94a74cee675883e8a4ae
andresjoeam <akademicandrew@gmail.com>
2024-07-19 23:11:47 -0400

    Implementing Drag and drop mouse function

--- src/test/java/Selenium/MouseActionsExample.java
+++ src/test/java/Selenium/MouseActionsExample.java
@@ -24,24 +24,26 @@ public class MouseActionsExample {
 
         Actions action1 = new Actions(driver);
         action1.moveToElement(driver.findElement(By.xpath("//*[@id=\"__next\"]/header/div[1]/nav/a[1]"))).build().perform();
-
         action1.moveToElement(driver.findElement(By.xpath("//*[@id=\"__next\"]/header/div[1]/nav/a[2]"))).build().perform();
-
         action1.moveToElement(driver.findElement(By.xpath("//*[@id=\"__next\"]/header/div[1]/nav/a[3]"))).build().perform();
-
         action1.moveToElement(driver.findElement(By.xpath("//*[@id=\"__next\"]/header/div[1]/nav/div"))).build().perform();
         // click on the loupe
         action1.moveToElement(driver.findElement(By.xpath("//button[@data-comp='CtaSearch']"))).click().build().perform();
         // type cooler and select the word
         action1.moveToElement(driver.findElement(By.xpath("//input[@role='search']"))).click().sendKeys("Cooler").doubleClick().build().perform();
-
         // in case to short the code line, the alternative below about DoubleClicl
         //action1.doubleClick(driver.findElement(By.xpath("//input[@role='search']")))
-
         action1.moveToElement(driver.findElement(By.xpath("//*[@id=\"__next\"]/aside[1]/div/div[1]/div/div/button"))).click().build().perform();
-
         //action1.moveToElement(driver.findElement(By.xpath("//*[@id=\"__next\"]/header/div[1]/nav/a[5]"))).contextClick().build().perform();
         //Same case if it is needed a shor code length
         action1.contextClick(driver.findElement(By.xpath("//*[@id=\"__next\"]/header/div[1]/nav/a[5]"))).build().perform();;
+
+
+
+        // ------------------------------------------ KeyBoard Actions  -----------------------------------------------//
+
+
+
+
     }
 }
